dda281bcc57fac6c09b5e6eb5ac8bb18c4efd12f
Ibrahim <ibrahim.tigrek@gmail.com>
2020-04-19 11:12:59 +0300

    now takes command line argument for url

--- adobe_connect_hack.py
+++ adobe_connect_hack.py
@@ -1,60 +1,71 @@
+import sys
+import selenium
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys 
 from selenium.webdriver.common.action_chains import ActionChains
-import selenium
 from credentials import USERNAME, PASSWORD
 
-browser = webdriver.Chrome("driver/chromedriver80.exe")
+def main():
 
-url = 'https://lms.sehir.edu.tr/mod/adobeconnect/view.php?id=64596'
-browser.get(url)
+    # get argument from command line
+    if len(sys.argv) == 1:
+        url = 'https://lms.sehir.edu.tr/mod/adobeconnect/view.php?id=64596'
+    else:
+        url = sys.argv[1]
 
-mysehir = browser.find_element_by_xpath("//div[@class='potentialidp']/a")
-mysehir.click()
+    browser = webdriver.Chrome("driver/chromedriver80.exe")
 
-username_field = browser.find_element_by_name("UserName")
-password_field = browser.find_element_by_name("Password")
+    browser.get(url)
 
-username_field.send_keys(USERNAME)
-password_field.send_keys(PASSWORD)
+    mysehir = browser.find_element_by_xpath("//div[@class='potentialidp']/a")
+    mysehir.click()
 
-browser.find_element_by_id("submitButton").click()
+    username_field = browser.find_element_by_name("UserName")
+    password_field = browser.find_element_by_name("Password")
 
-# MAIN PAGE REACHED
+    username_field.send_keys(USERNAME)
+    password_field.send_keys(PASSWORD)
 
-main_window = browser.window_handles[0]
-join_meeting_btn = browser.find_element_by_id('kati2')
-join_meeting_btn.click()
+    browser.find_element_by_id("submitButton").click()
 
-WebDriverWait(browser, 10).until(EC.number_of_windows_to_be(2))
-browser.switch_to.window(browser.window_handles[-1])
+    # MAIN PAGE REACHED
 
-# todo: fix
-browser.find_element_by_tag_name('a').click()
-try: 
-    kayitli_btn = browser.find_element_by_id('registeredTab')
-except selenium.common.exceptions.NoSuchElementException:
+    main_window = browser.window_handles[0]
+    join_meeting_btn = browser.find_element_by_id('kati2')
+    join_meeting_btn.click()
+
+    WebDriverWait(browser, 10).until(EC.number_of_windows_to_be(2))
+    browser.switch_to.window(browser.window_handles[-1])
+
+    # todo: fix
     browser.find_element_by_tag_name('a').click()
-    kayitli_btn = browser.find_element_by_id('registeredTab')
-kayitli_btn.click()
+    try: 
+        kayitli_btn = browser.find_element_by_id('registeredTab')
+    except selenium.common.exceptions.NoSuchElementException:
+        browser.find_element_by_tag_name('a').click()
+        kayitli_btn = browser.find_element_by_id('registeredTab')
+    kayitli_btn.click()
 
-# login in second time
-username_field = browser.find_element_by_name('login')
-password_field = browser.find_element_by_name('password')
-username_field.send_keys(USERNAME)
-password_field.send_keys(PASSWORD)
-browser.find_element_by_id('login-button').click()
+    # login in second time
+    username_field = browser.find_element_by_name('login')
+    password_field = browser.find_element_by_name('password')
+    username_field.send_keys(USERNAME)
+    password_field.send_keys(PASSWORD)
+    browser.find_element_by_id('login-button').click()
 
-app_btn = browser.find_element_by_class_name('open-in-app-button')
-browser.execute_script("arguments[0].click();", app_btn)
+    app_btn = browser.find_element_by_class_name('open-in-app-button')
+    browser.execute_script("arguments[0].click();", app_btn)
 
-## todo: fix
-actions = ActionChains(browser)
-actions.send_keys(Keys.LEFT)
-actions.send_keys(Keys.ENTER)
-actions.perform()
+    ## todo: fix
+    actions = ActionChains(browser)
+    actions.send_keys(Keys.LEFT)
+    actions.send_keys(Keys.ENTER)
+    actions.perform()
+
+if __name__ == '__main__':
+    main()
 
 
 
